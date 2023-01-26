525af795771a13831ea7c33afb16a4678f568c25
Mokwar <mokwar7@gmail.com>
2023-01-26 20:54:29 +0300

    Bug fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup')
-let formInputName = document.querySelector('.popup__input_name');
-let formInputDescription = document.querySelector('.popup__input_description');
+let formInputName = document.querySelector('.popup__input_type_name');
+let formInputDescription = document.querySelector('.popup__input_type_description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 let form = document.querySelector('.popup__form');
@@ -14,14 +14,14 @@ form.addEventListener('submit', formSubmit);
 function openPopup() {
     formInputName.value = profileName.textContent.trim();
     formInputDescription.value = profileDescription.textContent.trim();
-    popup.classList.add("popup__opened");
+    popup.classList.add("popup_opened");
 }
 function closePopup() {
-    popup.classList.remove("popup__opened");
+    popup.classList.remove("popup_opened");
 }
 function formSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = formInputName.value;
     profileDescription.textContent = formInputDescription.value;
-    popup.classList.remove("popup__opened");
+    closePopup();
 }
